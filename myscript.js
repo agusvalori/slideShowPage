7be8bd27a4365f7ae64eed81a73139a8c43dba94
agusvalori <61027212+agusvalori@users.noreply.github.com>
2022-04-12 19:08:18 -0300

    Agregando comentarios

--- myscript.js
+++ myscript.js
@@ -1,32 +1,35 @@
+console.log("Iniciando Programa");
 
-console.log("Iniciando Programa")
+var selectSlider = 0;//Index del arreglo imagenes
+var imagenes = []; // arreglo con los src de las imagenes
+window.addEventListener("load", function () {
+  imagenes[0] = "archivos/image/image1.jpg";
+  imagenes[1] = "archivos/image/image2.jpg";
+  imagenes[2] = "archivos/image/image3.jpg";
+  imagenes[3] = "archivos/image/image4.jpg";
+  imagenes[5] = "archivos/image/image4.jpg";
+  this.document.slider.src = imagenes[selectSlider];
+});
 
-var selectSlider = 0;
-var imagenes = [];
-window.addEventListener("load", function(){        
-    imagenes[0]="archivos/image/image1.jpg"
-    imagenes[1]="archivos/image/image2.jpg"
-    imagenes[2]="archivos/image/image3.jpg"
-    imagenes[3]="archivos/image/image4.jpg"
-    imagenes[5]="archivos/image/image4.jpg"
-    this.document.slider.src =imagenes[selectSlider];
+const desplazarDerecha = () => {
+  console.log("Tendria que desplazar a la derecha: ", selectSlider);
+  if (selectSlider < imagenes.length - 1) {
+    selectSlider++;
+  } else {
+    selectSlider = 0;
+  }
+};
 
-  
-})
+/* 
+al cambiar el index del arreglo donde tengo guardadas las rutas de las imagenes no me cambia las imagenes.
 
+*/
 
-const desplazarDerecha = ()=>{
-    console.log("Tendria que desplazar a la derecha: ",selectSlider)
-    if(selectSlider<imagenes.length-1)
-    {
-        selectSlider++;
-    }else{selectSlider=0}
-}
-
-const desplazarIzquierda = ()=>{
-    console.log("Tendria que desplazar a la izquierda: ",selectSlider)
-    if(selectSlider>0)
-    {
-        selectSlider--;
-    }else{selectSlider=6}
-}
+const desplazarIzquierda = () => {
+  console.log("Tendria que desplazar a la izquierda: ", selectSlider);
+  if (selectSlider > 0) {
+    selectSlider--;
+  } else {
+    selectSlider = 6;
+  }
+};
